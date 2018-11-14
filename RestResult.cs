790d1b6978dcb856083e3bfa43ab1548a4f648d0
s.zorin <s.zorin@logexpert.ru>
2018-11-14 16:07:18 +0300

    modify result by docs

--- RestResult.cs
+++ RestResult.cs
@@ -3,6 +3,21 @@ using Newtonsoft.Json;
 namespace FlespiSharp{
     public sealed class RestResult<T>{
         [JsonProperty("result")]
-        public T Value;
+        public T[] Values;
+
+        [JsonProperty("errors", NullValueHandling = NullValueHandling.Ignore)]
+        public RestError[] Errors;
+    }
+
+    public sealed class RestError
+    {
+        [JsonProperty("code")]
+        public int Code;
+
+        [JsonProperty("id")]
+        public int Id;
+
+        [JsonProperty("reason")]
+        public string Reason;
     }
 }
