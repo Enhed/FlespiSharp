c5f61bc6ac18cfe43e42ad63e3dad15708753e5c
s.zorin <s.zorin@logexpert.ru>
2018-11-14 16:07:40 +0300

    add rest provider for simly dev services api

---
+++ Http/RestProvider.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+
+namespace FlespiSharp.Http{
+    public abstract class RestProvider{
+        private readonly Connection connection;
+
+        protected RestProvider(Connection connection) =>
+            this.connection = connection ?? throw new System.ArgumentNullException(nameof(connection));
+
+        protected Task<RestResult<T>> Get<T>(string uri){
+            return GetRestResult<T>( x => x.GetAsync(uri) );
+        }
+
+        protected Task<RestResult<T>> Post<T, TInput>(TInput data, string uri){
+            var content = CreateContent(data);
+            return GetRestResult<T>( x => x.PostAsync(uri, content) );
+        }
+
+        protected Task<RestResult<T>> Put<T, TInput>(TInput data, string uri){
+            var content = CreateContent(data);
+            return GetRestResult<T>( x => x.PutAsync(uri, content) );
+        }
+
+        private HttpContent CreateContent<T>(T data){
+            var json = JsonConvert.SerializeObject(data);
+            return new StringContent(json, Encoding.UTF8, "application/json");
+        }
+
+        private async Task<RestResult<T>> GetRestResult<T>(Func<HttpClient, Task<HttpResponseMessage>> getter){
+            using(var response = (await getter(connection.GetAuthenticatedHttpClient())).EnsureSuccessStatusCode())
+                    return JsonConvert.DeserializeObject<RestResult<T>>(await response.Content.ReadAsStringAsync());
+        }
+    }
+}
