4278e7c7a73363ef25903d5ce0448dc40700883b
s.zorin <s.zorin@logexpert.ru>
2018-11-14 12:58:20 +0300

    add connection

---
+++ Connection.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace FlespiSharp
+{
+    public sealed class Connection : IDisposable
+    {
+        private const string headerName = "FlespiToken";
+        private const string baseAddress = "https://flespi.io";
+        private readonly string token;
+        private readonly HttpClient httpClient;
+
+        public Connection(string token){
+
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new ArgumentException("message", nameof(token));
+            }
+
+            this.token = token;
+
+            httpClient = new HttpClient{
+                BaseAddress = new Uri(baseAddress)
+            };
+
+            var header = new AuthenticationHeaderValue(headerName, token);
+            httpClient.DefaultRequestHeaders.Authorization = header;
+        }
+
+        public HttpClient GetAuthenticatedHttpClient() => httpClient;
+
+        public void Dispose()
+        {
+            httpClient.Dispose();
+        }
+    }
+}
