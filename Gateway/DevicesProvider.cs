dd29cb392c2291514f24b7c15b235cb618606cc6
s.zorin <s.zorin@logexpert.ru>
2018-11-15 16:58:07 +0300

    add device provider

---
+++ Gateway/DevicesProvider.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FlespiSharp.Http;
+
+namespace FlespiSharp.Gateway
+{
+    public sealed class DevicesProvider : RestProvider
+    {
+        public DevicesProvider(HttpClient client, PathBuilder builder)
+            : base(client, builder.AppendPath("devices"))
+        {
+        }
+
+        public Task<RestResult<Device>> Create(Device[] devices, string parameters = null){
+            return Post<Device, Device[]>(devices, parameters);
+        }
+
+        public Task<RestResult<Device>> Create(Device device, string parameters = null){
+            return Create( new [] { device }, parameters );
+        }
+
+        public DevicesService Ident(string value) => Selector($"ident={value}");
+
+        public DevicesService All => Selector("all");
+        public DevicesService Ids(int[] values)
+            => Selector(string.Join(",", values.Select(v => v.ToString())));
+        public DevicesService Id(int value) => Selector(value.ToString());
+        public DevicesService Name(string name) => Selector($"name={name}");
+
+        public DevicesService Selector(string selector)
+            => new DevicesService(client, builder.AppendPath(selector));
+    }
+}
