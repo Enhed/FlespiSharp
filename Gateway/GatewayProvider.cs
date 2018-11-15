8c5a3f816abf0d3f20c1038e76b05d5029b02717
s.zorin <s.zorin@logexpert.ru>
2018-11-15 16:58:19 +0300

    add gw provider

---
+++ Gateway/GatewayProvider.cs
@@ -0,0 +1,20 @@
+using System.Net.Http;
+using FlespiSharp.Http;
+
+namespace FlespiSharp.Gateway
+{
+    public sealed class GatewayProvider : PathProvider
+    {
+        public GatewayProvider(HttpClient client)
+            : base(client, PathBuilder.Create("gw"))
+        {
+        }
+
+        public DevicesProvider Devices => new DevicesProvider(client, builder);
+    }
+
+    public static class GatewayExtension{
+        public static GatewayProvider CreateGateway(this Connection connection)
+            => new GatewayProvider(connection.GetAuthenticatedHttpClient());
+    }
+}
