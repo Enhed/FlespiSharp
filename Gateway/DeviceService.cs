c9b2bdaeaf4b4ae04fa2970de252cd39440798d6
s.zorin <s.zorin@logexpert.ru>
2018-11-15 11:12:08 +0300

    add deleting & refactor

--- Gateway/DeviceService.cs
+++ Gateway/DeviceService.cs
@@ -4,56 +4,48 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using FlespiSharp.Http;
-using Newtonsoft.Json;
 
-namespace FlespiSharp.Gateway{
+namespace FlespiSharp.Gateway
+{
     public class DeviceService : RestProvider
     {
         public DeviceService(Connection connection) : base(connection) { }
 
         private const string basePath = "gw/devices";
 
-        public Task<RestResult<Device>> Get(string selector = "all", string parameters = null){
+        public Task<RestResult<Device>> Get(string selector = "all", string parameters = null)
+            => Get<Device>($"{basePath}/{selector}{(parameters == null ? null : $"?{parameters}")}");
+
+        public Task<RestResult<Device>> Get(int[] ids, string parameters = null){
+            var selector = string.Join(",", ids.Select(id => id.ToString()));
             return Get<Device>($"{basePath}/{selector}{(parameters == null ? null : $"?{parameters}")}");
         }
+
+        public Task<RestResult<Device>> Get(int id, string parameters = null)
+            => Get( new [] { id }, parameters );
 
         public Task<RestResult<Device>> Create(Device[] devices, string parameters = null){
             return Post<Device, Device[]>(devices,
                 parameters == null ? basePath : $"{basePath}?{parameters}");
         }
 
-        public Task<RestResult<Device>> Create(Device device, string parameters = null){
-            return Create(new [] { device }, parameters);
-        }
-    }
+        public Task<RestResult<Device>> Create(Device device, string parameters = null)
+            => Create(new [] { device }, parameters);
 
-    public class Device
-    {
-        [JsonProperty("device_type_id", NullValueHandling = NullValueHandling.Ignore)]
-        public int? TypeId;
-
-        [JsonProperty("ident", NullValueHandling = NullValueHandling.Ignore)]
-        public string Ident;
-
-        [JsonProperty("messages_ttl", NullValueHandling = NullValueHandling.Ignore)]
-        public int? MessagesSecondsTTL;
-
-        [JsonProperty("name", NullValueHandling = NullValueHandling.Ignore)]
-        public string Name;
-
-        [JsonProperty("phone", NullValueHandling = NullValueHandling.Ignore)]
-        public string Phone;
-
-        [JsonIgnore]
-        public TimeSpan? MessagesTTL{
-            get{
-                return MessagesSecondsTTL == null
-                    ? (TimeSpan?) null
-                    : TimeSpan.FromSeconds(MessagesSecondsTTL.Value);
+        public Task<RestResult<Device>> Delete(string selector, string parameters = null){
+            if (string.IsNullOrWhiteSpace(selector))
+            {
+                throw new ArgumentException("message", nameof(selector));
             }
-            set{
-                MessagesSecondsTTL = (int) value?.TotalSeconds;
-            }
+
+            return Delete<Device>($"{basePath}/{selector}{(parameters == null ? null : $"?{parameters}")}");
         }
+
+        public Task<RestResult<Device>> Delete(int[] ids, string parameters = null){
+            var selector = string.Join(",", ids.Select(id => id.ToString()));
+            return Delete(selector, parameters);
+        }
+
+        public Task<RestResult<Device>> Delete(int id, string parameters = null) => Delete(new [] { id }, parameters);
     }
 }
